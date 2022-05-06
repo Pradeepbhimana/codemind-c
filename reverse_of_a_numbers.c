383f9a13b3ae5ba542162b6e10b621d3eb2bfab8
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-06 15:14:13 +0530

    #include<stdio.h> #include<math.h> int main(){     int a,b,c=0,sum;     scanf("%d",&a);     b=a;     while(b!=0){
            c=c*10+b%10;
            
            b/=10;
        }
        printf("%d",c);
        
            
    }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b,c=0,sum;
+    scanf("%d",&a);
+    b=a;
+    while(b!=0){
+        
+        c=c*10+b%10;
+        
+        b/=10;
+    }
+    printf("%d",c);
+    
+        
+}
