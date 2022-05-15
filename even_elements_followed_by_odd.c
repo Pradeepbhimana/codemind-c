ea847a17be3fd5d631cd78ad49b1398a483ed83c
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-15 19:53:55 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b[a];     for (int i=0;i<a;i++){         scanf("%d",&b[i]);     }     int c,d;     scanf("%d%d",&c,&d);     int k=0;     for (int i=0;i<a;i++){         if(b[i]%2==0){             printf("%d ",b[i]);         }
        }
        for (int i=0;i<a;i++){
            if(b[i]%2!=0){
                printf("%d ",b[i]);
            }
            
        }
        
        
    }

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b[a];
+    for (int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+    int c,d;
+    scanf("%d%d",&c,&d);
+    int k=0;
+    for (int i=0;i<a;i++){
+        if(b[i]%2==0){
+            printf("%d ",b[i]);
+        }
+        
+    }
+    for (int i=0;i<a;i++){
+        if(b[i]%2!=0){
+            printf("%d ",b[i]);
+        }
+        
+    }
+    
+    
+}
