92bc3bc924d4518441de755ee41ec48eebdde717
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-15 21:17:10 +0530

    #include<stdio.h> int main(){
        int a;
        scanf("%d",&a);
        int b[a];
        for(int i=0;i<a;i++){
            scanf("%d",&b[i]);
        }
       /* printf("%d ",a);
         for(int i=0;i<a;i++){
            printf("%d ",b[i]);
        }*/
        int k;
        int q=0,sum=0;
        for(int i=0;i<a;i++){
            for(int j=i;j<a;j++){
                if(i!=j){
                    if(b[i]==b[j]){
                        for(int k=j;k<a;k++){
                            b[k]=b[k+1];
                        }
                        a--;
                        j--;
                    }
                    
                    
                }
            }
        }
        for(int i=0;i<a;i++){
            if(b[i]%2!=0){
                sum+=1;
            }
        }
        printf("%d",sum);
        
    }

---
+++ odd_unique_elements_count.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    
+    int a;
+    scanf("%d",&a);
+    int b[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+   /* printf("%d ",a);
+     for(int i=0;i<a;i++){
+        printf("%d ",b[i]);
+    }*/
+    int k;
+    int q=0,sum=0;
+    for(int i=0;i<a;i++){
+        for(int j=i;j<a;j++){
+            if(i!=j){
+                if(b[i]==b[j]){
+                    for(int k=j;k<a;k++){
+                        b[k]=b[k+1];
+                    }
+                    a--;
+                    j--;
+                }
+                
+                
+            }
+        }
+    }
+    for(int i=0;i<a;i++){
+        if(b[i]%2!=0){
+            sum+=1;
+        }
+    }
+    printf("%d",sum);
+    
+}
