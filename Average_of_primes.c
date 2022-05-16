a47086974d7e3435e8be4164daefdad1a59796ad
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-16 14:33:19 +0530

    #include<stdio.h> int prime(int a){     int k=0;     if(a==1){         return 0;     }     else{
            for(int i=2;i<a;i++){
                if(a%i==0){
                    k++;
                
                }
            }
            if(k==0){
                return 1;
            }
            else{
                return 0;
            }
        }
        
    }
    int main(){
        int a;
        scanf("%d",&a);
        int b[a];
        float sum=0,k=0;
        for(int i=0;i<a;i++){
            scanf("%d",&b[i]);
            if(prime(b[i])){
                sum+=b[i];
                k++;
            }
        }
        printf("%.2f",sum/k);
        
        
    }

---
+++ Average_of_primes.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int prime(int a){
+    int k=0;
+    if(a==1){
+        return 0;
+    }
+    else{
+        
+        for(int i=2;i<a;i++){
+            if(a%i==0){
+                k++;
+            
+            }
+        }
+        if(k==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    
+}
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b[a];
+    float sum=0,k=0;
+    for(int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+        if(prime(b[i])){
+            sum+=b[i];
+            k++;
+        }
+    }
+    printf("%.2f",sum/k);
+    
+    
+}
