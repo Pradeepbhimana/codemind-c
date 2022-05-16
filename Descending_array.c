c1d485e96e680dc859cb380cc09601ecfd0e3f90
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-16 19:28:24 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int c[a];     for(int i=0;i<a;i++){         scanf("%d",&c[i]);
        }
        /*int d[b];
        for(int i=0;i<b;i++){
            scanf("%d",&d[i]);
        }*/
        
        int q,s=0;
        for(int i=0;i<a;i++){
            if(c[i+1]-c[i]>0){
                s++;
                break;
                
            }
            
        
        }
        if(s==0){
            printf("yes");
            
        }
        else{
            printf("no");
        }
        /*for(int i=0;i<b;i++){
            q=0;
            for(int j=0;j<a;j++){
                if(d[i]==c[j]){
                    q++;
                }
            }
            if(q==0){
                s++;
            }
        }
       printf("%d",s);*/
        
        
        
            
    }

---
+++ Descending_array.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int c[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&c[i]);
+        
+    }
+    /*int d[b];
+    for(int i=0;i<b;i++){
+        scanf("%d",&d[i]);
+    }*/
+    
+    int q,s=0;
+    for(int i=0;i<a;i++){
+        if(c[i+1]-c[i]>0){
+            s++;
+            break;
+            
+        }
+        
+    
+    }
+    if(s==0){
+        printf("yes");
+        
+    }
+    else{
+        printf("no");
+    }
+    /*for(int i=0;i<b;i++){
+        q=0;
+        for(int j=0;j<a;j++){
+            if(d[i]==c[j]){
+                q++;
+            }
+        }
+        if(q==0){
+            s++;
+        }
+    }
+   printf("%d",s);*/
+    
+    
+    
+        
+}
