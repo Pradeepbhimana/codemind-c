2863f4fcd27c78ffa01bf5b33ef5c4567f7ac988
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-16 13:59:35 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b[a];     for(int i=0;i<a;i++){         scanf("%d",&b[i]);     }      for(int i=0;i<a;i++){         for(int j=i;j<a;j++){             if(i!=j){                 if(b[i]==b[j]){                     for(int k=j;k<a;k++){                         b[k]=b[k+1];                     }                     a--;                     j--;                 }
                }
            }
        }
        for(int i=0;i<a;i++){
            printf("%d ",b[i]);
        }
    }

---
+++ unique_elements.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+     for(int i=0;i<a;i++){
+        for(int j=i;j<a;j++){
+            if(i!=j){
+                if(b[i]==b[j]){
+                    for(int k=j;k<a;k++){
+                        b[k]=b[k+1];
+                    }
+                    a--;
+                    j--;
+                }
+                
+                
+            }
+        }
+    }
+    for(int i=0;i<a;i++){
+        printf("%d ",b[i]);
+    }
+}
