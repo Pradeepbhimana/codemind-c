0e9fd29859d35b90f31f592549b443da09f5d8d8
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-17 20:22:03 +0530

    #include<stdio.h> int main(){     int a,b;     scanf("%d%d",&a,&b);     int c[a][b];     for(int i=0;i<a;i++){         for(int j=0;j<b;j++){             scanf("%d",&c[i][j]);         }     }     int sum=0;     //if(a%2==0){     for(int i=1;i<a-1;i++){         for(int j=1;j<b-1;j++){
                sum+=c[i][j];
                    //printf("%d " ,c[i][j]);
                
            }
        }
        //}
        printf("%d",sum);
    }

---
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int c[a][b];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            scanf("%d",&c[i][j]);
+        }
+    }
+    int sum=0;
+    //if(a%2==0){
+    for(int i=1;i<a-1;i++){
+        for(int j=1;j<b-1;j++){
+        
+            sum+=c[i][j];
+                //printf("%d " ,c[i][j]);
+            
+        }
+    }
+    //}
+    printf("%d",sum);
+}
