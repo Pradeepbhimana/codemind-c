d088e5a34b912726e88631b95ca5417e30481818
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-17 10:54:28 +0530

    #include <stdio.h> int rev(int a){     if(a==0){         return 1;     }     int b=a,c=0;     while(b!=0){         c+=1;         b/=10;     }     return c; }
    int main()
    {
        int a,c=0;
        scanf("%d",&a);
        int b[a];
        for (int i=0;i<a;i++){
            scanf("%d",&b[i]);
        }
        int k=rev(b[0]);
        
        for(int i=0;i<a;i++){
            if(k<rev(b[i])){
               k=rev(b[i]);
               
           }
           //printf("%d   ",b[i]);
           
            
        }
        for(int i=0;i<a;i++){
            if(k==rev(b[i])){
                c++;
            }
        }
        printf("%d",c);
        
    
        return 0;
    }

---
+++ Max_digits_count.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+int rev(int a){
+    if(a==0){
+        return 1;
+    }
+    int b=a,c=0;
+    while(b!=0){
+        c+=1;
+        b/=10;
+    }
+    return c;
+}
+
+int main()
+{
+    int a,c=0;
+    scanf("%d",&a);
+    int b[a];
+    for (int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+    int k=rev(b[0]);
+    
+    for(int i=0;i<a;i++){
+        if(k<rev(b[i])){
+           k=rev(b[i]);
+           
+       }
+       //printf("%d   ",b[i]);
+       
+        
+    }
+    for(int i=0;i<a;i++){
+        if(k==rev(b[i])){
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+
+    return 0;
+}
