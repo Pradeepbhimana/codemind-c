55901ada75edecc0f52033a05c5a71d8c904b8be
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-17 21:16:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int a;     scanf("%d",&a);     int arr[a][a];     int b[a][a];     int max=0,sum=0;     for(int i=0;i<a;i++)     {         for(int j=0;j<a;j++)         {             scanf("%d",&arr[i][j]);             //sum+=arr[i][j];         }     }     int c[a][a];     for(int i=0;i<a;i++)     {         for(int j=0;j<a;j++)         {             scanf("%d",&b[i][j]);             //sum+=arr[i][j];         }     }     for(int i=0;i<a;i++)     {         for(int j=0;j<a;j++)         {             c[i][j]=abs(arr[i][j]-b[i][j]);             //sum+=arr[i][j];         }
        }
        for(int i=0;i<a;i++)
        {
            for(int j=0;j<a;j++)
            {
                if(j<a-1){
                    printf("%d ", c[i][j]);
                //sum+=arr[i][j];
                }
                else{
                    printf("%d", c[i][j]);
                }
            }
            if(i<a-1){
                printf("
    ");
            }
            
        }
        
       /* int max=0,sum=0;
        for(int j=0;j<b;j++){
            for(int i=0;i<a;i++)
            {
                if(arr[i][j]%2==0){
                    max+=arr[i][j];
                }
                else{
                    sum+=arr[i][j];
                }
            }
            
        }
        printf("%d %d",max,sum);*/
        //printf("%d",sum);
    }

---
+++ Absolute_difference_of_two_matrices.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a][a];
+    int b[a][a];
+    int max=0,sum=0;
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<a;j++)
+        {
+            scanf("%d",&arr[i][j]);
+            //sum+=arr[i][j];
+        }
+    }
+    int c[a][a];
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<a;j++)
+        {
+            scanf("%d",&b[i][j]);
+            //sum+=arr[i][j];
+        }
+    }
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<a;j++)
+        {
+            c[i][j]=abs(arr[i][j]-b[i][j]);
+            //sum+=arr[i][j];
+        }
+        
+    }
+    for(int i=0;i<a;i++)
+    {
+        for(int j=0;j<a;j++)
+        {
+            if(j<a-1){
+                printf("%d ", c[i][j]);
+            //sum+=arr[i][j];
+            }
+            else{
+                printf("%d", c[i][j]);
+            }
+        }
+        if(i<a-1){
+            printf("
+");
+        }
+        
+    }
+    
+   /* int max=0,sum=0;
+    for(int j=0;j<b;j++){
+        for(int i=0;i<a;i++)
+        {
+            if(arr[i][j]%2==0){
+                max+=arr[i][j];
+            }
+            else{
+                sum+=arr[i][j];
+            }
+        }
+        
+    }
+    printf("%d %d",max,sum);*/
+    //printf("%d",sum);
+}
