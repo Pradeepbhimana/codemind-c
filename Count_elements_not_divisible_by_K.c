3af3d4d2bed3fc37bb8a4717eb71c58b1b0c5729
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-05-17 10:22:29 +0530

    #include <stdio.h>
    int main()
    {
        int a,c;
        scanf("%d%d",&a,&c);
        int b[a];
        for (int i=0;i<a;i++){
            scanf("%d",&b[i]);
        }
        int k=0;
        
        for(int i=0;i<a;i++){
            if(b[i]%c!=0){
                k++;
            }
            
        }
        printf("%d",k);
        
    
        return 0;
    }

---
+++ Count_elements_not_divisible_by_K.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,c;
+    scanf("%d%d",&a,&c);
+    int b[a];
+    for (int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+    int k=0;
+    
+    for(int i=0;i<a;i++){
+        if(b[i]%c!=0){
+            k++;
+        }
+        
+    }
+    printf("%d",k);
+    
+
+    return 0;
+}
