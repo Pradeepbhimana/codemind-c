f30760206db01b3dac77e1b4e084350d78e82eef
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-12 15:24:19 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b[a],d=0,max=0;     for (int i=0;i<a;i++){         scanf("%d",&b[i]);     }     for(int i=0;i<a;i++){         int c=0;         for (int j=0;j<a;j++){             if(b[i]>b[j]){                 c++;             }         }         printf("%d ",c);     }
    }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b[a],d=0,max=0;
+    for (int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<a;i++){
+        int c=0;
+        for (int j=0;j<a;j++){
+            if(b[i]>b[j]){
+                c++;
+            }
+        }
+        printf("%d ",c);
+    }
+    
+    
+}
