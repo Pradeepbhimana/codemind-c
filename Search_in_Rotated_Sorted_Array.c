7b0c74108fc221d7a9d4de48582e8f55cc6286d0
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-06-12 15:51:03 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b[a];     for (int i=0;i<a;i++){         scanf("%d",&b[i]);     }     int s,m=0,c[1000];     scanf("%d",&s);     for (int i=0;i<a;i++){         if(b[i]==s){             printf("%d ",i);             m=1;             break;
            }
            
        }
        if(m==0){
            printf("-1");
        }
     
    }

---
+++ Search_in_Rotated_Sorted_Array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b[a];
+    for (int i=0;i<a;i++){
+        scanf("%d",&b[i]);
+    }
+    int s,m=0,c[1000];
+    scanf("%d",&s);
+    for (int i=0;i<a;i++){
+        if(b[i]==s){
+            printf("%d ",i);
+            m=1;
+            break;
+            
+        }
+        
+    }
+    if(m==0){
+        printf("-1");
+    }
+ 
+}
