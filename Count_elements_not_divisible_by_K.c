b4b03315524ba85eb134cd561f1c2b9b18e2e7a1
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-07 11:24:31 +0530

    #include<stdio.h> int co(int a){     int s=0;     if(a==0){         return 1;
        }
        else{
            if(a<0){
                a=-a;
            }
            while(a!=0){
                s+=1;
                a/=10;
            }
            return s;
        }
        
    }
    int main(){
        int a,s;
        
        scanf("%d %d",&a,&s);
        int b[a];
        for (int i=0;i<a;i++){
            scanf("%d",&b[i]);
        }
        int q=0;
        for (int i=0;i<a;i++){
            if(b[i]%s!=0){
                q+=1;
            }
        }
        printf("%d",q);
    }

--- Count_elements_not_divisible_by_K.c
+++ Count_elements_not_divisible_by_K.c
@@ -1,24 +1,36 @@
-
-#include <stdio.h>
-
-int main()
-{
-    int a,c;
-    scanf("%d%d",&a,&c);
+#include<stdio.h>
+int co(int a){
+    int s=0;
+    if(a==0){
+        return 1;
+        
+        
+    }
+    else{
+        if(a<0){
+            a=-a;
+        }
+        while(a!=0){
+            s+=1;
+            a/=10;
+        }
+        return s;
+    }
+    
+}
+int main(){
+    int a,s;
+    
+    scanf("%d %d",&a,&s);
     int b[a];
     for (int i=0;i<a;i++){
         scanf("%d",&b[i]);
     }
-    int k=0;
-    
-    for(int i=0;i<a;i++){
-        if(b[i]%c!=0){
-            k++;
+    int q=0;
+    for (int i=0;i<a;i++){
+        if(b[i]%s!=0){
+            q+=1;
         }
-        
     }
-    printf("%d",k);
-    
-
-    return 0;
+    printf("%d",q);
 }
