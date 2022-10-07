af31d68077b2508c55034ed87ed20e829dbe3bdc
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-07 11:53:16 +0530

    #include<stdio.h> int co(int a){     int s=0;     while(a){         s=s+a%10;         a/=10;     }     return s;
    }
    int main(){
        int a,p;
        
        scanf("%d%d",&a,&p);
        int b[a];
        for (int i=0;i<a;i++){
            scanf("%d",&b[i]);
        }
        int q[p];
        for (int i=0;i<p;i++){
            scanf("%d",&q[i]);
        }
        int c[1000],o=0;
        for (int i=0;i<a;i++){
            for (int j=0;j<p;j++){
                if(b[i]==q[j]){
                    c[o]=b[i];
                    o++;
                }
            }
                
        }
        for(int i=0;i<o;i++){
           for(int j = i+1; j < o; j++){
              if(c[i] == c[j]){
                 for(int k = j; k <o; k++){
                    c[k] = c[k+1];
                 }
                 j--;
                 o--;
              }
           }
        }
        printf("%d",o);
              
        
       
    }

--- Count_elements_I.c
+++ Count_elements_I.c
@@ -1,57 +1,48 @@
 #include<stdio.h>
+int co(int a){
+    int s=0;
+    while(a){
+        s=s+a%10;
+        a/=10;
+    }
+    return s;
+    
+}
 int main(){
-    int a,b;
-    scanf("%d%d",&a,&b);
-    int c[a];
-    for(int i=0;i<a;i++){
-        scanf("%d",&c[i]);
-        
+    int a,p;
+    
+    scanf("%d%d",&a,&p);
+    int b[a];
+    for (int i=0;i<a;i++){
+        scanf("%d",&b[i]);
     }
-    int d[b];
-    for(int i=0;i<b;i++){
-        scanf("%d",&d[i]);
+    int q[p];
+    for (int i=0;i<p;i++){
+        scanf("%d",&q[i]);
     }
-    for(int i=0;i<a;i++){
-        for(int j=i;j<a;j++){
-            if(i!=j){
-                if(c[i]==c[j]){
-                    for(int k=j;k<a;k++){
-                        c[k]=c[k+1];
-                    }
-                    a--;
-                    j--;
-                }
-                
-                
+    int c[1000],o=0;
+    for (int i=0;i<a;i++){
+        for (int j=0;j<p;j++){
+            if(b[i]==q[j]){
+                c[o]=b[i];
+                o++;
             }
         }
+            
     }
-    for(int i=0;i<b;i++){
-        for(int j=i;j<b;j++){
-            if(i!=j){
-                if(d[i]==d[j]){
-                    for(int k=j;k<b;k++){
-                        d[k]=d[k+1];
-                    }
-                    b--;
-                    j--;
-                }
-                
-                
-            }
-        }
+    for(int i=0;i<o;i++){
+       for(int j = i+1; j < o; j++){
+          if(c[i] == c[j]){
+             for(int k = j; k <o; k++){
+                c[k] = c[k+1];
+             }
+             j--;
+             o--;
+          }
+       }
     }
-    int q=0;
-    for(int i=0;i<a;i++){
-        for(int j=0;j<b;j++){
-            if(c[i]==d[j]){
-                q++;
-            }
-        }
-    }
-    printf("%d",q);
+    printf("%d",o);
+          
     
-    
-    
-        
+   
 }
