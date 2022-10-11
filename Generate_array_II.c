d303775e692a2b290188edda29a6eaaaed187de4
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-11 13:33:31 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b[a];     for(int i=0;i<a;i++){         scanf("%d",&b[i]);
        }
        for(int i=0;i<a;i+=2){
            
            for (int j=0;j<b[i+1];j++){
                printf("%d ",b[i]);
            }
            
           
        }
    }

--- Generate_array_II.c
+++ Generate_array_II.c
@@ -2,42 +2,17 @@
 int main(){
     int a;
     scanf("%d",&a);
-    int c[a];
+    int b[a];
     for(int i=0;i<a;i++){
-        scanf("%d",&c[i]);
+        scanf("%d",&b[i]);
         
     }
-    /*int d[b];
-    for(int i=0;i<b;i++){
-        scanf("%d",&d[i]);
-    }*/
-    
-    int q,s=0;
     for(int i=0;i<a;i+=2){
-        if(c[i+1]==0){
-            continue;
-        }
-        else{
-            for(int j=0;j<c[i+1];j++){
-                printf("%d ",c[i]);
-            }
-        }
-    
-    }
-    /*for(int i=0;i<b;i++){
-        q=0;
-        for(int j=0;j<a;j++){
-            if(d[i]==c[j]){
-                q++;
-            }
-        }
-        if(q==0){
-            s++;
-        }
-    }
-   printf("%d",s);*/
-    
-    
-    
         
+        for (int j=0;j<b[i+1];j++){
+            printf("%d ",b[i]);
+        }
+        
+       
+    }
 }
