cd691a8e131e30369e6cf5e4c1766c19f3c626e2
Pradeepbhimana <97426572+Pradeepbhimana@users.noreply.github.com>
2022-10-11 16:45:11 +0530

    #include<stdio.h> int main(){     int a,b;     scanf("%d %d",&a,&b);     int m[a][b];
        for(int i=0;i<a;i++){
            for (int j=0;j<b;j++){
                scanf("%d",&m[i][j]);
            }
        }
        int su=0;
        for(int i=0;i<a;i++){
            for (int j=0;j<b;j++){
                if(i==j or i+j==a-1){
                    su+=m[i][j];
                }
            }
        }
        printf("%d",su);
        
    }

--- sum_of_Diagonal_values.c
+++ sum_of_Diagonal_values.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 int main(){
     int a,b;
-    scanf("%d%d",&a,&b);
-    int c[a][b];
+    scanf("%d %d",&a,&b);
+    int m[a][b];
+    
     for(int i=0;i<a;i++){
-        for(int j=0;j<b;j++){
-            scanf("%d",&c[i][j]);
+        for (int j=0;j<b;j++){
+            scanf("%d",&m[i][j]);
         }
     }
-    int sum=0;
-    //if(a%2==0){
+    int su=0;
     for(int i=0;i<a;i++){
-        for(int j=0;j<b;j++){
-            if(i==j || i+j==a-1){
-                sum+=c[i][j];
-                //printf("%d " ,c[i][j]);
+        for (int j=0;j<b;j++){
+            if(i==j or i+j==a-1){
+                su+=m[i][j];
             }
         }
     }
-    //}
-    printf("%d",sum);
+    printf("%d",su);
+    
 }
